d4ebab36a221f146a6293f83b6f02abcd142b94f
brisibe <jbrisibe@gmail.com>
2021-11-17 09:49:00 +0000

    restructured header folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './shared/layouts/footer/footer.component';
+import { FooterComponent } from './shared/layouts/header/header.component';
 
 @NgModule({
   declarations: [
